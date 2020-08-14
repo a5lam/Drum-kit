9f6b1e2c7b31bf1c181f1590e5810777dac73eeb
aslamshaikh524 <aslamshaikh524@gmail.com>
2020-08-13 21:42:03 -0400

    Tweak audio playing

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return;// stop function to running all together
+    audio.currentTime = 0;
     audio.play();
 });
