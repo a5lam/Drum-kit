ee4a245e1dae9bde0abf97a21c8cbc9838346edd
aslamshaikh524 <aslamshaikh524@gmail.com>
2020-08-13 21:49:37 -0400

    key animation added

--- Script.js
+++ Script.js
@@ -3,4 +3,8 @@ window.addEventListener('keydown', function(e){
     if(!audio) return;// stop function to running all together
     audio.currentTime = 0;
     audio.play();
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+
 });
